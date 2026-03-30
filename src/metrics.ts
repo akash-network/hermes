59346e346e9159f0b965d40da593316716d3913a
Sergii <sergiy.stotskiy@gmail.com>
2026-03-30 12:58:57 +0000

    feat: add price staleness gauge to metrics

--- src/metrics.ts
+++ src/metrics.ts
@@ -8,3 +8,7 @@ export const hermesFetchDuration = meter.createHistogram("hermes.price_fetch_dur
     description: "Duration of Hermes API price fetch in milliseconds",
     unit: "ms",
 });
+export const priceStaleness = meter.createGauge("hermes.price_staleness", {
+    description: "How far behind the on-chain price is from the latest Pyth price",
+    unit: "s",
+});
